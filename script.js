0476786f26605a85ab713d56a5e4abf798d71836
Amil Ali <amil13985@gmail.com>
2023-09-08 12:47:32 +0530

    add

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let rect = document.getElementById("rectangle");
 rect.addEventListener("mousemove", (del)=>{
     console.log(del);
 })
+
+rect.addEventListener("mousemove", ()=>{
+    rect.style.backgroundColor = "blue";
+})
+
