8d8c177faec67a79421bf31a1d50eb0794aac8aa
Amil Ali <amil13985@gmail.com>
2023-09-08 12:48:01 +0530

    add

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ rect.addEventListener("mousemove", ()=>{
     rect.style.backgroundColor = "blue";
 })
 
+rect.addEventListener("mouseout", ()=>{
+    rect.style.backgroundColor = "green";
+})
+rect.addEventListener("dblclick", ()=>{
+    rect.style.backgroundColor = "yellow";
+})
