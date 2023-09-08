d54cee945c8d993b10f2f63418519669e9ef79f7
Amil Ali <amil13985@gmail.com>
2023-09-08 22:35:32 +0530

    ADD

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 let rect = document.getElementById("rectangle");
 
 rect.addEventListener("mousemove", (del)=>{
-    console.log(del);
-})
+    let RectLoc = rect.getBoundingClientRect();
+    let insideRec = del.clientX;
+    let difference =   insideRec - RectLoc.left;
 
-rect.addEventListener("mousemove", ()=>{
-    rect.style.backgroundColor = "blue";
-})
+    if(difference < RectLoc.width/2)
+    {
+        let RedColor = gsap.utils.mapRange(0,RectLoc.width/2,255,0, difference);
+        gsap.to(rect,{
+            backgroundColor: `rgb(${RedColor},0,0)`,
+            ease: "Power4",
+        });
+    }
+    else {
+        // Reverse the calculation for blueColor
+        let blueColor = gsap.utils.mapRange(RectLoc.width / 2, RectLoc.width, 0, 255, difference);
+        gsap.to(rect, {
+            backgroundColor: `rgb(0,0,${blueColor})`,
+            ease: "Power4",
+        });
+    }
 
-rect.addEventListener("mouseout", ()=>{
-    rect.style.backgroundColor = "green";
-})
-rect.addEventListener("dblclick", ()=>{
-    rect.style.backgroundColor = "yellow";
-})
+});
+
+// rect.addEventListener("mousemove", ()=>{
+//     rect.style.backgroundColor = "blue";
+// })
+
+// rect.addEventListener("mouseout", ()=>{
+//     rect.style.backgroundColor = "green";
+// })
+// rect.addEventListener("dblclick", ()=>{
+//     rect.style.backgroundColor = "yellow";
+// })
