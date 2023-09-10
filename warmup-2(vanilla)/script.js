a089a65fcf08aa78653735fcdb0601847a18a0ea
Amil Ali <amil13985@gmail.com>
2023-09-10 15:27:34 +0530

    add

--- warmup-2(vanilla)/script.js
+++ warmup-2(vanilla)/script.js
@@ -1,15 +1,17 @@
 let imge = document.getElementById("imge");
 let rec = document.getElementById("rectangle");
 
-
-
 window.addEventListener("mousemove", (det) => {
-  var mapper = gsap.utils.mapRange(0, window.innerWidth, 100+imge.getBoundingClientRect().width/2, window.innerWidth-(100+imge.getBoundingClientRect().width/2), det.clientX);
+  var mapperx = gsap.utils.mapRange(0, window.innerWidth, 100+imge.getBoundingClientRect().width/2, window.innerWidth-(100+imge.getBoundingClientRect().width/2), det.clientX);
+
+
+  var mappery = gsap.utils.mapRange(0, window.innerHeight, 100+imge.getBoundingClientRect().height/2, window.innerHeight-(100+imge.getBoundingClientRect().height/2), det.clientY);
+
 
   gsap.to("#imge", {
-    left: mapper + "px",
+    left: mapperx + "px",
     ease: Power3,
-    top: det.clientY + "px"
+    top: mappery + "px"
   });
 });
 
